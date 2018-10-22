cf82a73ec2c7b408655549eb437d197b14d7def1
barstow123 <39165000+barstow123@users.noreply.github.com>
2018-10-22 11:58:59 -0500

    'f'

--- Django/main/apps/book_authors/models.py
+++ Django/main/apps/book_authors/models.py
@@ -8,10 +8,10 @@ class Book(models.Model):
 	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
-		return f'{self.name}'
+		#return f'{self.name}'
 
 	def __repr__(self):
-		return f'Book(name={self.name}, desc={self.desc}, created_at{self.created_at}, updated_at{self.updated_at})'
+		#return f'Book(name={self.name}, desc={self.desc}, created_at{self.created_at}, updated_at{self.updated_at})'
 
 class Author(models.Model):
 	first_name = models.CharField(max_length=255)
@@ -23,7 +23,7 @@ class Author(models.Model):
 	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
-		return f'{self.first_name} {self.last_name}'
+		#return f'{self.first_name} {self.last_name}'
 
 	def __repr__(self):
-		return f'Author(first_name={self.first_name}, last_name={self.last_name}, email={self.email}, created_at={self.created_at}, updated_at={self.updated_at})'
+		#return f'Author(first_name={self.first_name}, last_name={self.last_name}, email={self.email}, created_at={self.created_at}, updated_at={self.updated_at})'
